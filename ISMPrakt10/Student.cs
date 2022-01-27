43521cfb70e31a2aaa78db84670888e32dd30b90
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-27 20:55:31 +0200

    Created class Student with initial structure

---
+++ ISMPrakt10/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt10
+{
+    public class Student: Human
+    {
+        public string Faculty { get; set; }
+        public string Grpoup { get; set; }
+        public string University { get; set; }
+        public int Course { get; set; }
+        public Student(string name, string surname, DateTime date, string faculty, 
+            string grpoup, string university, int course) : base(name, surname, date)
+        {
+            Faculty = faculty;
+            Grpoup = grpoup;
+            University = university;
+            Course = course;
+        }
+
+        public string GetOnlyUniversityData() 
+        {
+            return $"Faculty: {Faculty}\nGrpoup: {Grpoup}\n University: {University}\n Course: {Course}";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Faculty: {Faculty}\nGrpoup: {Grpoup}\n University: {University}\n Course: {Course}";
+        }
+    }
+}
