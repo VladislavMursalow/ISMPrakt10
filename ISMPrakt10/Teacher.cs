2f5d12bfd0435ad678eb8202613be67f065c08d0
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-27 21:24:32 +0200

    Create Teacher.cs with init structure

---
+++ ISMPrakt10/Teacher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt10
+{
+    public class Teacher: Human
+    {
+        public string Positions { get; set; }
+        public string Department { get; set; }
+        public string University { get; set; }
+        public Teacher(string name, string surname, DateTime date, string university,
+            string positions, string department) : base(name, surname, date)
+        {
+            Department = department;
+            Positions = positions;
+            University = university;
+        }
+
+        public string GetOnlyUniversityData()
+        {
+            return $"\nDepartment: {Department}\nSchool: {Positions}\nUniversity: {University}\n";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nDepartment: {Department}\nSchool: {Positions}\nUniversity: {University}\n";
+
+        }
+    }
+}
