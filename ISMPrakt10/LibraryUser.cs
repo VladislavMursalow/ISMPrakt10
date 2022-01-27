fb833a663caae5c382aaf62d35599b64ac2e884a
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-27 21:32:02 +0200

    Created LibraryUser.cs with init structure

---
+++ ISMPrakt10/LibraryUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt10
+{
+    public class LibraryUser : Human
+    {
+        public int ReaderNumber { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public int Contribution { get; set; }
+        public LibraryUser(string name, string surname, DateTime date, 
+            int readerNumb, DateTime dateOfIssue, int contribution) : base(name, surname, date)
+        {
+            ReaderNumber = readerNumb;
+            DateOfIssue = dateOfIssue;
+            Contribution = contribution;
+        }
+
+        public int ContributionPerYear() 
+        {
+            return 12 * Contribution;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nReaderNumber: {ReaderNumber}\nDateOfIssue: " +
+                $"{DateOfIssue.ToShortDateString()}\nContribution: {Contribution}\n";
+        }
+    }
+}
