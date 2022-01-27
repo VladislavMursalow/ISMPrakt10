aa0b28091d0cb1cc79cdf080962f7355d0536722
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-27 19:55:17 +0200

    Created class Human

---
+++ ISMPrakt10/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt10
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthdayDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}, Народився : {BirthdayDate.ToShortDateString()}";
+        }
+    }
+}
